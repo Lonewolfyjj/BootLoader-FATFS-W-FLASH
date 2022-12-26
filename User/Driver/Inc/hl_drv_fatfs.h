e782f9108c3c09d9c555bdf76c16d3009388ba52
jiujun <jiujun.yi@hollyland-tech.com>
2022-12-26 14:44:16 +0800

    ✨修改代码（Driver）：修改FATFS驱动代码   1、FATFS驱动功能封装   2、实现功能：读写文件，挂载，卸载，删除文件，获取文件的大小   结束

--- User/Driver/Inc/hl_drv_fatfs.h
+++ User/Driver/Inc/hl_drv_fatfs.h
@@ -28,6 +28,9 @@
 /* Includes ------------------------------------------------------------------*/
 
 #include "ff.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "string.h"
 
 /* typedef -------------------------------------------------------------------*/
 /* define --------------------------------------------------------------------*/
@@ -35,16 +38,117 @@
 /* 文件写入函数 */
 #define FATFS_DRV_WRITE_FUN     0
 /* 格式化函数 */
-#define FATFS_DRV_MKFS_FUN      0
+#define FATFS_DRV_MKFS_FUN      1
+
+#define FATFS_FUN_RET_ERR       -1
+#define FATFS_FUN_RET_OK        0
 
 /* variables -----------------------------------------------------------------*/
 /* Private function(only *.c)  -----------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
 
-void hl_drv_fatfs_read(char* filename);
+/**
+ * @brief 挂载文件系统
+ * 在外部SPI Flash挂载文件系统，文件系统挂载时会对SPI设备初始化,
+ * 初始化函数调用流程如下 
+ * f_mount()->find_volume()->disk_initialize->SPI_FLASH_Init() 
+ * @return int 
+ * @date 2022-12-24
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-12-24      <td>yijiujun     <td>新建
+ * </table>
+ */
+int hl_drv_fatfs_init();
 
-void hl_drv_fatfs_del_file(char* filename);
+/**
+ * @brief 卸载文件系统
+ * @return int 
+ * @date 2022-12-24
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-12-24      <td>yijiujun     <td>新建
+ * </table>
+ */
+int hl_drv_fatfs_deinit();
 
+/**
+ * @brief 读取文件的数据
+ * @param [in] filename 文件名
+ * @param [in] read_buffer 读缓冲区
+ * @param [in] data_size 读写数量
+ * @return int 成功返回读取到的字节数, 失败返回-1
+ * @date 2022-12-26
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-12-26      <td>yijiujun     <td>新建
+ * </table>
+ */
+int hl_drv_fatfs_read(char* filename, uint8_t* read_buffer, uint32_t data_size);
+
+#if (FATFS_DRV_WRITE_FUN)
+/**
+ * @brief 向文件的写入数据
+ * @param [in] filename 文件名
+ * @param [in] write_buffer 写数据缓冲区
+ * @param [in] data_size 读写数量
+ * @return int 成功返回写入成功的字节数, 失败返回-1
+ * @date 2022-12-26
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-12-26      <td>yijiujun     <td>新建
+ * </table>
+ */
+int hl_drv_fatfs_write(char* filename, uint8_t* write_buffer, uint32_t data_size);
+#endif
+
+/**
+ * @brief 删除一个文件
+ * @param [in] filename 文件名
+ * @return int 
+ * @date 2022-12-26
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-12-26      <td>yijiujun     <td>新建
+ * </table>
+ */
+int hl_drv_fatfs_del_file(char* filename);
+
+/**
+ * @brief 获取一个文件的大小
+ * @param [in] filename 
+ * @return int 成功返回文件大小， 失败返回-1
+ * @date 2022-12-26
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-12-26      <td>yijiujun     <td>新建
+ * </table>
+ */
+int hl_drv_fatfs_get_file_size(char* filename);
 #endif
 /*
  * EOF
--- User/Driver/Src/hl_drv_fatfs.c
+++ User/Driver/Src/hl_drv_fatfs.c
@@ -24,174 +24,196 @@
 /* Includes ------------------------------------------------------------------*/
 
 #include "hl_drv_fatfs.h"
-#include "n32l40x.h"
-#include "string.h"
-#include "stdio.h"
-#include "stdint.h"
 
 /* typedef -------------------------------------------------------------------*/
 /* define --------------------------------------------------------------------*/
 
-#define debug_printf     printf
+#define debug_printf printf
+#define FATFS_PATH "0:"  //文件系统的路径
 
 /* variables -----------------------------------------------------------------*/
 
-static FATFS fs;    //文件系统
-
-FIL fnew;      //文件对象
+static FATFS fat_fs;  //文件系统
+FIL          fnew;    //文件对象
 
 #if (FATFS_DRV_MKFS_FUN)
-BYTE  work[FF_MAX_SS];          //一定是一个全局变量
+BYTE work[FF_MAX_SS];  //一定是一个全局变量
 #endif
+
+static uint8_t _drv_init_flag = 0;
 
 /* Private function(only *.c)  -----------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
 
-void hl_drv_fatfs_read(char* filename)
+int hl_drv_fatfs_init(FATFS* fs)
 {
-    uint8_t  path_string[20] = "0:";
-    uint8_t  WriteBuffer[32] = { 0 }; /* 读缓冲区*/
-    UINT      fnum;    /* 读写数量 */
-    FRESULT      res_flash; /* 文件操作结果 */
+    FRESULT res_flash;  //文件操作结果
 
-    //在外部SPI Flash挂载文件系统，文件系统挂载时会对SPI设备初始化
-    //初始化函数调用流程如下
-    //f_mount()->find_volume()->disk_initialize->SPI_FLASH_Init()
-
-    res_flash = f_mount(&fs, "0:", 1);
-    debug_printf("res_mount 1 = %d\r\n", res_flash);
+    if (_drv_init_flag == 1) {
+        debug_printf("[erro] fatfs already init\r\n");
+        return FATFS_FUN_RET_ERR;
+    }
+    res_flash = f_mount(&fat_fs, FATFS_PATH, 1);
 
     if (res_flash == FR_NO_FILESYSTEM) {
         debug_printf("FLASH还没有文件系统, 即将进行格式化...\r\n");
 
 #if (FATFS_DRV_MKFS_FUN)
         /* 格式化 */
-        res_flash = f_mkfs("0:", 0, work, sizeof(work));
+        res_flash = f_mkfs(FATFS_PATH, 0, work, sizeof(work));
         if (res_flash == FR_OK) {
 
             debug_printf("--FLASH已成功格式化文件系统。\r\n");
 
             /* 格式化后，先取消挂载 */
-            res_flash = f_mount(RT_NULL, "0:", 1);
+            res_flash = f_mount(NULL, FATFS_PATH, 1);
             debug_printf("res_demount = %d\r\n", res_flash);
 
             /* 重新挂载	*/
-            res_flash = f_mount(&fs, "0:", 1);
-            debug_printf("res_remount = %d\r\n", res_flash);
-
+            res_flash = f_mount(&fat_fs, FATFS_PATH, 1);
+            if (res_flash == FR_OK) {
+                debug_printf("res_remount = %d\r\n", res_flash);
+                debug_printf("\r\n****** mount successful ******\r\n");
+            } else {
+                debug_printf("[remount failed] res_remount = %d\r\n", res_flash);
+            }
+            
         } else {
-
             debug_printf("《《格式化失败。》》\r\n");
         }
 #endif
     } else {
-        debug_printf("\r\n****** 即将进行文件读取测试... ******\r\n");
+        debug_printf("\r\n****** mount successful ******\r\n");
     }
 
-    strcat(path_string, filename);
-    debug_printf("path_string = %s\r\n", path_string);
+    if (res_flash != FR_OK) {
+        debug_printf("[error] res_mount = %d\r\n", res_flash);
+        return FATFS_FUN_RET_ERR;
+    }
+
+    _drv_init_flag = 1;
+    return FATFS_FUN_RET_OK;
+}
+
+int hl_drv_fatfs_deinit()
+{
+    FRESULT res_flash;  //文件操作结果
+
+    if (_drv_init_flag == 0) {
+        return FATFS_FUN_RET_ERR;
+    }
+    res_flash = f_mount(NULL, FATFS_PATH, 1);  //卸载文件系统
+    if (res_flash != FR_OK) {
+        return FATFS_FUN_RET_ERR;
+    }
+
+    _drv_init_flag = 0;
+    return FATFS_FUN_RET_OK;
+}
+
+int hl_drv_fatfs_read(char* filename, uint8_t* read_buffer, uint32_t data_size)
+{
+    FRESULT res_flash;  //文件操作结果
+    UINT     fnum;       //文件成功读写数量
+    char    path_string[20] = { FATFS_PATH };
+
+    if (_drv_init_flag == 0) {
+        debug_printf("[erro] fatfs no init\r\n");
+        return FATFS_FUN_RET_ERR;
+    }
+    strcat(path_string, filename);  //将磁盘路径和文件名称组合成一个文件路径
 
     res_flash = f_open(&fnew, path_string, FA_READ);  //只读
     debug_printf("res_open = %d\r\n", res_flash);
 
-    if (res_flash == FR_OK) {
-        debug_printf("--打开%s文件成功。\r\n", filename);
-
-        res_flash = f_read(&fnew, WriteBuffer, sizeof(WriteBuffer), &fnum);
-        debug_printf("--res_read = %d\r\n", res_flash);
-
-        debug_printf("--文件读取成功，读到字节数据：%d\r\n", fnum);
-
-        debug_printf("--从文件读到的数据为：\r\n%s\r\n", WriteBuffer);
-        /* 不再读写，关闭文件 */
-        f_close(&fnew);
-        
+    if (res_flash == FR_NO_FILE) {
+        return FATFS_FUN_RET_ERR;
     }
+
+    res_flash = f_read(&fnew, read_buffer, data_size, &fnum);
+
+    /* 不再读写，关闭文件 */
+    f_close(&fnew);
+    return fnum;
 }
 
 #if (FATFS_DRV_WRITE_FUN)
-void fatfs_write_test(int argc, char* argv[])
+int hl_drv_fatfs_write(char* filename, uint8_t* write_buffer, uint32_t data_size)
 {
-    char     path_string[20] = "0:";
-    char    *file_name       = argv;
-    uint8_t  WriteBuffer[32] = { 0 }; /* 写缓冲区*/
-    
-    int      fnum; /* 文件成功读写数量 */
-    FRESULT      res_flash; /* 文件操作结果 */
-    //在外部SPI Flash挂载文件系统，文件系统挂载时会对SPI设备初始化
-    //初始化函数调用流程如下
-    //f_mount()->find_volume()->disk_initialize->SPI_FLASH_Init()
+    UINT     fnum;       //文件成功读写数量
+    FRESULT res_flash;  //文件操作结果
+    char    path_string[20] = { FATFS_PATH };
 
-    res_flash = f_mount(&fs, "0:", 1);
-    debug_printf("res_mount 1 = %d\r\n", res_flash);
-
-    if (res_flash == FR_NO_FILESYSTEM) {
-        debug_printf("FLASH还没有文件系统, 即将进行格式化...\r\n");
-
-#if (FATFS_DRV_MKFS_FUN)
-        /* 格式化 */
-        //res_flash = f_mkfs("0:", 0, work, sizeof(work));
-        res_flash = FR_DISK_ERR;
-        if (res_flash == FR_OK) {
-            debug_printf("》FLASH已成功格式化文件系统。\r\n");
-            /* 格式化后，先取消挂载 */
-            res_flash = f_mount(RT_NULL, "0:", 1);
-            debug_printf("res_demount 2 = %d\r\n", res_flash);
-            /* 重新挂载	*/
-            res_flash = f_mount(&fs, "0:", 1);
-            debug_printf("res_mount 3 = %d\r\n", res_flash);
-        } else {
-            debug_printf("《《格式化失败。》》\r\n");
-        }
-#endif
-    } else {
-        debug_printf("\r\n****** 即将进行文件写入测试... ******\r\n");
+    if (_drv_init_flag == 0) {
+        debug_printf("[erro] fatfs no init\r\n");
+        return FATFS_FUN_RET_ERR;
     }
-
-    strcat(path_string, file_name);  //将磁盘路径和文件名称组合成一个文件路径
+    strcat(path_string, filename);  //将磁盘路径和文件名称组合成一个文件路径
 
     res_flash = f_open(&fnew, path_string, FA_CREATE_ALWAYS | FA_WRITE);  //创建和写入权限
     debug_printf("res_open = %d\r\n", res_flash);
 
-    if (res_flash == FR_OK) {
-        debug_printf("--打开/创建%s文件成功, 向文件写入数据。\r\n", argv);
-
-        /* 将指定存储区内容写入到文件内 */
-        res_flash = f_write(&fnew, argv[2], sizeof(argv[2]), &fnum);
-        debug_printf("res_write = %d\r\n", res_flash);
-
-        if (res_flash == FR_OK) {
-            debug_printf("文件写入成功，写入字节数据：%d\r\n", fnum);
-
-            debug_printf("向文件写入的数据为：\r\n%s\r\n", argv[2]);
-
-        } else {
-            debug_printf("！！文件写入失败：(%d)\n", res_flash);
-        }
-        /* 不再读写，关闭文件 */
-        f_close(&fnew);
-    } else {
-
-        debug_printf("！！打开/创建文件失败。\r\n");
+    if (res_flash != FR_OK) {
+        return FATFS_FUN_RET_ERR;
     }
+
+    /* 将指定存储区内容写入到文件内 */
+    res_flash = f_write(&fnew, write_buffer, data_size, &fnum);
+    debug_printf("res_write = %d\r\n", res_flash);
+
+    if (res_flash == FR_OK) {
+        debug_printf("文件写入成功，写入字节数据：%d\r\n", fnum);
+    } else {
+        debug_printf("！！文件写入失败：(%d)\n", res_flash);
+    }
+    /* 不再读写，关闭文件 */
+    f_close(&fnew);
+    return fnum;
 }
 #endif
 
-void hl_drv_fatfs_del_file(char* filename)
+int hl_drv_fatfs_del_file(char* filename)
 {
-    char path_string[20] = "0:";
-    FRESULT      res_flash; /* 文件操作结果 */
+    FRESULT res_flash;  //文件操作结果
+    char    path_string[20] = { FATFS_PATH };
+
+    if (_drv_init_flag == 0) {
+        debug_printf("[erro] fatfs no init\r\n");
+        return FATFS_FUN_RET_ERR;
+    }
+
     strcat(path_string, filename);  //将磁盘路径和文件名称组合成一个文件路径
 
-    res_flash = f_mount(&fs, "0:", 1);
-    debug_printf("res_mount = %d\r\n", res_flash);
-
-    f_unlink(path_string);
-    debug_printf("res_unlink = %d\r\n", res_flash);
+    res_flash = f_unlink(path_string);
+    debug_printf("unlink ret = %d\r\n", res_flash);
+    if (res_flash != FR_OK) {
+        return FATFS_FUN_RET_ERR;
+    }
 
     debug_printf("delete %s success\r\n", filename);
+
+    return FATFS_FUN_RET_OK;
 }
 
+int hl_drv_fatfs_get_file_size(char* filename)
+{
+    FRESULT res_flash;  //文件操作结果
+    FILINFO file_info;  //文件信息结构体
+    char    path_string[20] = { FATFS_PATH };
+
+    if (_drv_init_flag == 0) {
+        debug_printf("[erro] fatfs no init\r\n");
+        return FATFS_FUN_RET_ERR;
+    }
+    strcat(path_string, filename);  //将磁盘路径和文件名称组合成一个文件路径
+    res_flash = f_stat(filename, &file_info);
+    debug_printf("f_stat ret = %d\r\n", res_flash);
+    if (res_flash != FR_OK) {
+        return FATFS_FUN_RET_ERR;
+    }
+
+    return file_info.fsize;
+}
 
 /*
  * EOF
